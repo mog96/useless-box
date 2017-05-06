97bb9a4914a12722fae6e25fdfde6325c611a929
Mateo Garcia <mateog@stanford.edu>
2017-05-05 22:09:25 -0700

    Trick 1: Switch abuse. First implementation.

--- useless-box.ino
+++ useless-box.ino
@@ -3,12 +3,68 @@ int rightInverter = 3;
 int limitSwitch   = 4;
 int toggleSwitch  = 5;
 
+int switchToggledCount = 0;
+bool abusingSwitch = false;
+int kNumTogglesBeforeAbuse = 5;
+int cyclesSinceAbuseStart = 0;
+
+
 void setup() {
   pinMode(leftInverter, OUTPUT);
   pinMode(rightInverter, OUTPUT);
   pinMode(limitSwitch, INPUT_PULLUP);   // Input pullup keeps the switch 'high' when the circuit is open.
   pinMode(toggleSwitch, INPUT_PULLUP);  // (As opposed to reading an arbitrary value, or connecting a
                                         // resistor from the third pin of the switch to the Vin of the board.)
+  Serial.begin(115200);
+}
+
+void loop() {
+  int limitInput = digitalRead(limitSwitch);
+  int toggleInput = digitalRead(toggleSwitch);
+
+  if (switchToggledCount == kNumTogglesBeforeAbuse) {
+    abusingSwitch == true;
+    
+    Serial.println("START ABUSE");
+  }
+
+  if (abusingSwitch) {
+    cyclesSinceAbuseStart++;
+    
+    Serial.print("ABUSE CYCLES: ");
+    Serial.println(cyclesSinceAbuseStart);
+    
+    if (switchToggledCount == 0) {
+      cyclesSinceAbuseStart = 0;
+      abusingSwitch = false;
+    }
+    if (cyclesSinceAbuseStart % 50 == 0) {
+      
+      Serial.print("ABUSING ");
+      Serial.println(switchToggledCount);
+      
+      if (toggleInput == HIGH) {
+        fingerForward();
+        switchToggledCount--;
+      } else {
+        fingerReverse();
+      }
+    }
+    return;
+  }
+  
+  if (toggleInput == HIGH) {
+    fingerForward();
+  } else { // toggleInput == LOW {
+    if (limitInput == HIGH) {
+      fingerStop();
+    } else {
+      if (!abusingSwitch) {
+        switchToggledCount++;
+      }
+      fingerReverse();
+    }
+  }
 }
 
 void fingerForward() {
@@ -26,17 +82,3 @@ void fingerStop() {
   digitalWrite(rightInverter, HIGH);
 }
 
-void loop() {
-  int limitInput = digitalRead(limitSwitch);
-  int toggleInput = digitalRead(toggleSwitch);
-
-  if (toggleInput == HIGH) {
-    fingerForward();
-  } else { // toggleInput == LOW {
-    if (limitInput == HIGH) {
-      fingerStop();
-    } else {
-      fingerReverse();
-    }
-  }
-}
