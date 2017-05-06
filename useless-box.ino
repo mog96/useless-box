ae5583b52bc27e60756ecf38a247ce912bef8ef0
Mateo Garcia <mateog@stanford.edu>
2017-05-05 21:42:09 -0700

    Removes prints.

--- useless-box.ino
+++ useless-box.ino
@@ -9,8 +9,6 @@ void setup() {
   pinMode(limitSwitch, INPUT_PULLUP);   // Input pullup keeps the switch 'high' when the circuit is open.
   pinMode(toggleSwitch, INPUT_PULLUP);  // (As opposed to reading an arbitrary value, or connecting a
                                         // resistor from the third pin of the switch to the Vin of the board.)
-  Serial.begin(115200);
-  // Serial.println("READY");
 }
 
 void fingerForward() {
@@ -29,29 +27,8 @@ void fingerStop() {
 }
 
 void loop() {
-  if (Serial.available()) {
-    char userInput = Serial.read();
-    if (userInput == 'f') {
-      Serial.println("FORWARD");
-      fingerForward();
-    } else if (userInput == 'r') {
-      Serial.println("REVERSE");
-      fingerReverse();
-    } else if (userInput == 's') {
-      Serial.println("STOP");
-      fingerStop();
-    } else {
-      Serial.println("Invalid character.");
-    }
-  }
-
   int limitInput = digitalRead(limitSwitch);
-  Serial.print("LIMIT  ");
-  Serial.println(limitInput == HIGH ? "high" : "low");
-
   int toggleInput = digitalRead(toggleSwitch);
-  Serial.print("TOGGLE ");
-  Serial.println(toggleInput == HIGH ? "high" : "low");
 
   if (toggleInput == HIGH) {
     fingerForward();
