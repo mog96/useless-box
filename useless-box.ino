7eb879114384f250472503887bd12c022d62407a
Mateo Garcia <mateog@stanford.edu>
2017-05-06 22:03:08 -0700

    Functional version of battery-powered useless box.

--- useless-box.ino
+++ useless-box.ino
@@ -1,13 +1,26 @@
+/**
+ * Battery-powered implementation of the useless box
+ * using a Metro-Mini computer as a control mechanism.
+ * 
+ * IMPORTANT: When connected to USB, the finger will
+ * enter reverse mode. There is no code to stop the
+ * finger.
+ * 
+ * In a battery-powered environment, the Metro-Mini
+ * is disconnected from the battery when the finger
+ * hits the limit switch, so no 'stop' code is
+ * necessary.
+ */
+
 int leftInverter  = 2;
 int rightInverter = 3;
-int limitSwitch   = 4;
-int toggleSwitch  = 5;
+int toggleSwitch  = 4;
 
 void setup() {
   pinMode(leftInverter, OUTPUT);
   pinMode(rightInverter, OUTPUT);
-  pinMode(limitSwitch, INPUT_PULLUP);   // Input pullup keeps the switch 'high' when the circuit is open.
-  pinMode(toggleSwitch, INPUT_PULLUP);  // (As opposed to reading an arbitrary value, or connecting a
+  pinMode(toggleSwitch, INPUT_PULLUP);  // Input pullup keeps the switch 'high' when the circuit is open.
+                                        // (As opposed to reading an arbitrary value, or connecting a
                                         // resistor from the third pin of the switch to the Vin of the board.)
 }
 
@@ -21,22 +34,12 @@ void fingerReverse() {
   digitalWrite(rightInverter, HIGH);
 }
 
-void fingerStop() {
-  digitalWrite(leftInverter, HIGH);
-  digitalWrite(rightInverter, HIGH);
-}
-
 void loop() {
-  int limitInput = digitalRead(limitSwitch);
   int toggleInput = digitalRead(toggleSwitch);
 
   if (toggleInput == HIGH) {
     fingerForward();
   } else { // toggleInput == LOW {
-    if (limitInput == HIGH) {
-      fingerStop();
-    } else {
-      fingerReverse();
-    }
+    fingerReverse();
   }
 }
